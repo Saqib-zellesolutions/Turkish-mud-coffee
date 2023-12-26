17d634c0d03146304467d8b2f435e0788f26c3ce
saqibfaisal <saqibfaisalofficial@gmail.com>
2023-12-26 14:25:26 -0800

    middlewear changes

--- dbMiddlewear.js
+++ dbMiddlewear.js
@@ -1,9 +1,10 @@
 // dbMiddleware.js
 const mongoose = require("mongoose");
-
+const dotenv = require("dotenv");
+dotenv.config({ path: "./.env" });
 const connectToDatabase = async (req, res, next) => {
   const branch = req.params.branch;
-  const DBURI = process.env[`MONGODB_URL_BRANCH${branch.toUpperCase()}`];
+  const DBURI = process.env[`MONGODB_URL_BRANCH${branch.toUpperCase()}`]+'?retryWrites=true&w=majority';
   console.log(DBURI, "dbUrl");
   if (!DBURI) {
     console.error("DBURI is undefined. Check your environment variables.");
@@ -16,7 +17,7 @@ const connectToDatabase = async (req, res, next) => {
     await mongoose.connect(DBURI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
-      serverSelectionTimeoutMS: 10000,
+      // serverSelectionTimeoutMS: 10000,
     });
     console.log(`Connected to database for branch: ${branch}`);
     next();
